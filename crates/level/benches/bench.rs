72d608d0297e918f9e33c3c18a4f24a97facc5fd
Bjarke Bondo Andersen <andersen.bjarke@gmail.com>
2024-08-27 02:02:53 +0200

    level: reintroduce generation benchmark

--- crates/level/benches/bench.rs
+++ crates/level/benches/bench.rs
@@ -1,19 +1,19 @@
 use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
-// use std::hint::black_box;
+use std::hint::black_box;
 
 use dnd_rs_level::{Level, Solver};
 
-// fn run_level_generation_benchmark(c: &mut Criterion) {
-//     let mut seed = 0;
-//     c.bench_function("generate 50x50 level", |b| {
-//         b.iter(|| {
-//             seed += 1;
-//             fastrand::seed(seed);
+fn run_level_generation_benchmark(c: &mut Criterion) {
+    let mut seed = 0;
+    c.bench_function("generate 50x50 level", |b| {
+        b.iter(|| {
+            seed += 1;
+            fastrand::seed(seed);
 
-//             Level::random(black_box(50), black_box(50))
-//         })
-//     });
-// }
+            Level::random(black_box(50), black_box(50))
+        })
+    });
+}
 
 fn run_solver_benchmarks(c: &mut Criterion) {
     fastrand::seed(1337);
@@ -28,7 +28,7 @@ fn run_solver_benchmarks(c: &mut Criterion) {
 
 criterion_group!(
     benches,
-    // run_level_generation_benchmark,
+    run_level_generation_benchmark,
     run_solver_benchmarks,
 );
 criterion_main!(benches);
