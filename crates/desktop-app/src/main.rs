df2cad8aa56a7456f2187d5c83f7e5ad786a70fc
Bjarke Bondo Andersen <andersen.bjarke@gmail.com>
2024-08-29 02:32:15 +0200

    web: add required asset meta files

--- crates/desktop-app/src/main.rs
+++ crates/desktop-app/src/main.rs
@@ -5,6 +5,14 @@ use dnd_rs_plugin::DungeonsAndDiagramsPlugin;
 
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins, DungeonsAndDiagramsPlugin))
+        .add_plugins((
+            DefaultPlugins,
+            // To update asset meta run with `cargo run --features bevy/asset_processor`
+            // DefaultPlugins.set(AssetPlugin {
+            //     mode: AssetMode::Processed,
+            //     ..Default::default()
+            // }),
+            DungeonsAndDiagramsPlugin,
+        ))
         .run();
 }
